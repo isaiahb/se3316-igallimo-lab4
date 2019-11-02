4e13ce3c2b5834c66a18c3f0a18696d99c0e7c75
isaiah <isaiahballah@gmail.com>
2019-11-01 21:57:07 -0400

    api service wip

---
+++ client/src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Item } from "./item";
+
+export const items: Item[] = [
+	{ id: 11, name: 'Dr Nice', type: "book", loanPeriod: 10 },
+	{ id: 11, name: 'Mr Nobody', type: "book", loanPeriod: 100 },
+	{ id: 11, name: 'Dr Not So Nice', type: "cd", loanPeriod: 1 },
+];
+
+@Injectable({providedIn: 'root'})
+export class ConfigService {
+	constructor(private http: HttpClient) { }
+	getItems(): Item[] {
+		return items;
+	}
+}
+
--- client/src/app/item/item.component.ts
+++ client/src/app/item/item.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from "../item";
 
 @Component({
 	selector: 'app-item',
 	templateUrl: './item.component.html',
 	styleUrls: ['./item.component.scss']
 })
+
 export class ItemComponent implements OnInit {
 	name = 'the cool book';
 	id = '9001';
 	loanPeriod = 10;
 	type = 'book';
+
+	// constructor(private heroService: HeroService) { }
 
 	// constructor(id, name, loanPeriod, type) { 
 	// 	this.id = id;
